e67cd3282a6a171d4fe8dd79c447475411c74b13
Eran Schweitzer <eran.schweitzer@pnnl.gov>
2024-05-24 12:42:11 -0700

    Improved Prescient testing and output
    Output is now provided from solved Egret models as well.
    script to run test is also better.

--- data_model_tests/README.md
+++ data_model_tests/README.md
@@ -7,4 +7,20 @@ To run this file, Prescient needs to be installed, as well as.
 To install Prescient clone [this repository](https://github.com/grid-parity-exchange/Prescient) and follow the instructions [here](https://github.com/grid-parity-exchange/Prescient?tab=readme-ov-file#installing-from-source)
 
 Follow the [RTS-GMLC Example](https://github.com/grid-parity-exchange/Prescient?tab=readme-ov-file#rts-gmlc-example-model) to create the `downloads/rts_gmlc` folder.
+Note, the `rts_gmlc.py` script clones a repository, if the path where it plans on cloning has a space in it (e.g. if you're working on the PNNL One-Drive) this will cause an error.
+The solution is to modify line 63 of that file to (note that added double quotes around `rtsgmlc_path`):
+```python
+clone_cmd = 'git clone -n '+url+' '+'"'+rtsgmlc_path+'"'
+```
 The `data_path` argument in `sim_rts_gmlc_save_egret_test.py` needs to point to the `/downloads/rts_gmlc/RTS-GMLC/RTS_Data/SourceData/` folder.
+It can be passed to the script using the optional `--data-path` input.
+If you cloned Prescient to the same folder as the pyenergymarket repository, the default should be correct.
+
+The results from the run are commited to the repository for reference in the [`rts_gmlc_output`](./rts_gmlc_output/) folder.
+This includes both the prescient results as well as the json outputs of the mode data.
+There are outputs both before and after each model run (unit commitment RUC and real time SCED).
+The other csvs in the folder are results coming from Prescient.
+The idea is to try to set up independent Egret runs and compare them to these results for validation.
+
+>**IMPORTANT**<br>
+>It appears that the UC/dispatch problems in Egret don't automatically solve for LMPs, that is, we'll need to tack on a second step for that in all instances.
--- data_model_tests/sim_rts_gmlc_save_egret_test.py
+++ data_model_tests/sim_rts_gmlc_save_egret_test.py
@@ -2,6 +2,7 @@ from prescient.simulator import Prescient
 import argparse
 from types import ModuleType
 from egret.data.model_data import ModelData
+from prescient.simulator.data_manager import RucPlan
 import os
 # from pyomo.common.config import ConfigDict
 
@@ -15,6 +16,15 @@ def save_ruc_data(prescient_options, simulator, deterministic_ruc_instance:Model
         fname = f"ruc_modeldata{i}.json"
     deterministic_ruc_instance.write(fname)
 
+def save_ruc_sol(prescient_options, simulator, ruc_plan:RucPlan, ruc_date, ruc_hour):
+    """save data moel after solution"""
+    i = 0
+    fname = f"ruc_solved_modeldata{i}.json"
+    while os.path.exists(fname):
+        i +=1
+        fname = f"ruc_solved_modeldata{i}.json"
+    ruc_plan.deterministic_ruc_instance.write(fname)
+
 def save_sced_data(prescient_options, simulator, deterministic_sced_instance:ModelData):
     # Try going into debug mode here to find the right way to get modelData out of the sced instance
     i = 0
@@ -25,21 +35,37 @@ def save_sced_data(prescient_options, simulator, deterministic_sced_instance:Mod
 
     deterministic_sced_instance.write(fname)
 
+def save_sced_sol(prescient_options, simulator, deterministic_sced_instance:ModelData, deterministic_pricing_model:ModelData):
+    # Try going into debug mode here to find the right way to get modelData out of the sced instance
+    i = 0
+    fname = f"sced_solved_modeldata{i}.json"
+    fname2 = f"sced_pricing_modeldata{i}.json"
+    while os.path.exists(fname):
+        i += 1
+        fname = f"sced_solved_modeldata{i}.json"
+        fname2 = f"sced_pricing_modeldata{i}.json"
+
+    deterministic_sced_instance.write(fname)
+    deterministic_pricing_model.write(fname2)
+
 def get_configuration(key:str):
     return None
 
 def register_plugins(context, options, plugin_config):
     context.register_before_ruc_solve_callback(save_ruc_data)
+    context.register_after_ruc_generation_callback(save_ruc_sol)
     context.register_before_operations_solve_callback(save_sced_data)
+    context.register_after_operations_callback(save_sced_sol)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="test prescient")
     parser.add_argument("solver", nargs="?", choices=["gurobi", "cbc", "scip"], help="solver to use", default="gurobi")
+    parser.add_argument("--data-path", help="data path to RTS-GMLC/RTS_Data/SourceData/", default="../../Prescient/downloads/rts_gmlc/RTS-GMLC/RTS_Data/SourceData/")
     args = parser.parse_args()
     # set some options
     prescient_options = {
-            "data_path":"../downloads/rts_gmlc/RTS-GMLC/RTS_Data/SourceData/",
+            "data_path":args.data_path,
             "input_format":"rts-gmlc",
             "simulate_out_of_sample":True,
             "run_sced_with_persistent_forecast_errors":True,
