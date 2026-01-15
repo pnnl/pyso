db3f816188c99e4d89110a275399e9354918edfa
Eran Schweitzer <eran.schweitzer@pnnl.gov>
2026-01-15 11:09:41 -0800

    Adding timezone capability to EnergyMarket
    The timezone can now be specified in the time section of the configuration.
    if the convert_to_utc flag is set to TRUE (default) then the time range
    will be converted to UTC before passing to the DataProvider.
    
    IMPORTANT:
    the current configuration means that the time keys of the OUTPUTS
    may be different from the time keys for the INPUTS.
    For example, if the time zone is US/Eastern and the DataProvider expects
    UTC. then the input time ranges will be in US/Eastern, but the time keys
    in the Egret results will be in UTC.

--- src/pyenergymarket/pyenergymarket_defaults.py
+++ src/pyenergymarket/pyenergymarket_defaults.py
@@ -4,7 +4,9 @@ energymarket_defaults = {
         "dateto": None,
         "min_freq": 60, # period length in minutes
         "window": 24, # solution window
-        "lookahead": 0 # solution lookahead
+        "lookahead": 0, # solution lookahead
+        "tz": None, # specify time zone for INPUT dates
+        "convert_to_utc": True
     },
     "simulation": {
         "price_model": "lmp", # can be "lmp" (fix commitment), 
