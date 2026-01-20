eaecd692ad99c8bcf72b4de12892cdf0f428f08d
Eran Schweitzer <eran.schweitzer@pnnl.gov>
2026-01-19 17:59:14 -0800

    adding all parsers to __init__

--- src/pyenergymarket/__init__.py
+++ src/pyenergymarket/__init__.py
@@ -4,7 +4,12 @@ from .engine import EnergyMarket
 
 with suppress(ImportError):
     from .parsers.gvparser import GVParse
+with suppress(ImportError):
     from .parsers.pwparser import PWParse
+with suppress(ImportError):
+    from .parsers.egretparser import EgretProvider, DailyEgretProvider
+with suppress(ImportError):
+    from .parsers.naermparser import NAERMProvider
 from . import utils
 
-__all__ = ["EnergyMarket", "GVParse", "PWParse", "utils"]
+__all__ = ["EnergyMarket", "GVParse", "PWParse", "EgretProvider", "DailyEgretProvider", "NAERMProvider", "utils"]
